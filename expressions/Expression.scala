a586318b700cb11d69547fafccf7f0dac47d026b
SergLeft <sleftheriotis@yahoo.com>
2026-02-10 01:48:59 +0100

    Refactor TableRecord into Expression hierarchy

--- expressions/Expression.scala
+++ expressions/Expression.scala
@@ -1,56 +1,40 @@
-package dbms.v2.store
+package expressions
 
-import dbms.v2.misc.Variant
 
-/** Represents an individual immutable record
- * 
- *  @param elems a sequence of (attribute, value) tuples
- */ 
-class TableRecord(elems: Iterable[(String, Variant)]) extends Iterable[(String, Variant)] {
-  if (elems.isEmpty)
-    throw IllegalArgumentException("cannot create a record without attributes")
+sealed abstract class Expression {
 
-  val schema: Schema = Schema(elems.map((attribute: String, value: Variant) => attribute -> value.dataType))
-
-  /** Maps each attribute of the record to its value. */
-  protected val attributes: Map[String, Variant] = elems.toMap
-
-  /** Returns the number of attributes of this record. */
-  def numAttributes: Int = elems.size
-
-  /** Returns true iff the record contains the given attribute. */
-  def hasAttribute(attribute: String): Boolean = attributes.contains(attribute)
-
-  /** Returns the value of a specific attribute.
-   *
-   *  @param attribute the name of the attribute to get the value for
-   *  @return the value which corresponds to this attribute.
-   *  @throws IllegalArgumentException if the passed attribute is unknown.
-   */
-  def getValue(attribute: String): Variant = {
-    attributes.getOrElse(attribute, throw IllegalArgumentException("attribute name is unknown"))
+  /** Returns a string representation of this [[Expression]] */
+  override def toString: String = this match {
+    case Variable(name) => name
+    case Constant(value) => value.toString
+    case UnaryOperation(op, ex) => s"($op $ex)"
+    case BinaryOperation(op, left, right) => s"($left $op $right)"
   }
-
-  /** Returns the value of a specific attribute.
-   *
-   *  @param attribute the name of the attribute to get the value for
-   *  @return the value which corresponds to this attribute.
-   *  @throws IllegalArgumentException if the passed attribute is unknown.
-   */
-  def apply(attribute: String): Variant = getValue(attribute)
-
-  override def iterator: Iterator[(String, Variant)] = attributes.iterator
-
-  /** Returns the textual representation of the record. */
-  override def toString: String = attributes.mkString("(", ", ", ")")
-
-  /** Returns whether an object equals this record. */
-  override def equals(that: Any): Boolean = {
-    that match {
-      case record: TableRecord => record.attributes == this.attributes
-      case _ => false
-    }
-  }
-
-  override def hashCode: Int = attributes.hashCode
 }
+
+/** Represents a variable, i.e. a value to be set later.
+ *
+ * @param name the name of the variable
+ */
+case class Variable(name: String) extends Expression
+
+/** Represents a constant, i.e. a fixed value.
+ *
+ * @param value the value of the constant
+ */
+case class Constant(value: Double) extends Expression
+
+/** Represents any operation that takes a single argument.
+ *
+ * @param op a symbol representing the operator
+ * @param ex the argument
+ */
+case class UnaryOperation(op: String, ex: Expression) extends Expression
+
+/** Represents any operation that takes two arguments.
+ *
+ * @param op    a symbol representing the operator
+ * @param left  the left argument
+ * @param right the right argument
+ */
+case class BinaryOperation(op: String, left: Expression, right: Expression) extends Expression
